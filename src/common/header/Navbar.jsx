bee8d5e06cfa5bab2448a61f8df424cf617c1fb6
bidhanroyjoy <bidhan19994@gmail.com>
2022-04-14 04:24:47 +0600

    layout done

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
- 
+  // Toogle Menu
   const [MobileMenu, setMobileMenu] = useState(false)
   return (
     <>
